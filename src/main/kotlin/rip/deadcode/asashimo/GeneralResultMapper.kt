528ef1bc6d5b4bbde4ed2801ed2fa80b18051700
minebreaker <takutokoyama.terraflopsjapan@gmail.com>
2018-01-02 23:17:08 +0900

    GeneralResultMapperの実装第一弾

--- src/main/kotlin/rip/deadcode/asashimo/GeneralResultMapper.kt
+++ src/main/kotlin/rip/deadcode/asashimo/GeneralResultMapper.kt
@@ -1,12 +1,153 @@
 package rip.deadcode.asashimo
 
-import java.sql.ResultSet
+import com.google.common.annotations.VisibleForTesting
+import com.google.common.base.CaseFormat.*
+import org.slf4j.LoggerFactory
+import java.beans.Introspector
+import java.io.InputStream
+import java.io.Reader
+import java.math.BigDecimal
+import java.math.BigInteger
+import java.net.URL
+import java.sql.*
 import kotlin.reflect.KClass
 
+@Suppress("UNCHECKED_CAST")
 object GeneralResultMapper {
 
-    fun <T> map(cls: KClass<out Any>, resultSet: ResultSet): T {
-        TODO()
+    private val logger = LoggerFactory.getLogger(GeneralResultMapper::class.java)
+
+    fun <T : Any> map(cls: KClass<T>, resultSet: ResultSet): T {
+        return convertToBasicType(cls, resultSet)
+                ?: convertWithAllArgsConstructor(cls, resultSet)
+                ?: convertAsBean(cls, resultSet)
+                ?: throw AsashimoException("Failed to map ResultSet to class '${cls}'")
+    }
+
+    /**
+     * JDBC型が要求されていた場合、対応するメソッドを使用して値を取得する.
+     */
+    @VisibleForTesting
+    internal fun <T : Any> convertToBasicType(cls: KClass<T>, resultSet: ResultSet): T? {
+        return try {
+            @Suppress("IMPLICIT_CAST_TO_ANY")
+            return when (cls) {
+            // Directly provided by JDBC driver
+                java.sql.Array::class -> resultSet.getArray(1)
+                BigDecimal::class -> resultSet.getBigDecimal(1)
+                InputStream::class -> resultSet.getBinaryStream(1)
+                Blob::class -> resultSet.getBlob(1)
+                Boolean::class -> resultSet.getBoolean(1)
+                Byte::class -> resultSet.getByte(1)
+                ByteArray::class -> resultSet.getBytes(1)
+                Reader::class -> resultSet.getCharacterStream(1)
+                Clob::class -> resultSet.getClob(1)
+                java.sql.Date::class -> resultSet.getDate(1)
+                Double::class -> resultSet.getDouble(1)
+                Float::class -> resultSet.getFloat(1)
+                Int::class -> resultSet.getInt(1)
+                Long::class -> resultSet.getLong(1)
+                Short::class -> resultSet.getShort(1)
+                SQLXML::class -> resultSet.getSQLXML(1)
+                String::class -> resultSet.getString(1)
+                Time::class -> resultSet.getTime(1)
+                Timestamp::class -> resultSet.getTimestamp(1)
+                URL::class -> resultSet.getURL(1)
+
+            // Manual conversion
+                BigInteger::class -> resultSet.getBigDecimal(1).toBigInteger()
+                else -> null
+            } as T
+        } catch (e: Exception) {
+            when (e) {
+                is SQLException -> throw e
+                else -> {
+                    logger.trace("", e)
+                    null
+                }
+            }
+        }
+    }
+
+    @VisibleForTesting
+    internal fun <T : Any> convertWithAllArgsConstructor(cls: KClass<T>, resultSet: ResultSet): T? {
+
+        return try {
+            val resultSize = resultSet.metaData.columnCount
+            // cls.constructors requires kotlin-reflect library. Use old Java reflection.
+            val constructors = cls.java.declaredConstructors
+
+            val sameSizeConstructors = constructors.filter { it.parameterCount == resultSize }
+            for (constructor in sameSizeConstructors) {
+                // TODO check metadata to infer appropriate constructor
+                try {
+                    val types = constructor.parameterTypes
+                    val args = arrayOfNulls<Any>(types.size)
+                    for ((i, type) in types.withIndex()) {
+                        args[i] = resultSet.getObject(i + 1, type)
+                    }
+                    if (!constructor.isAccessible) constructor.isAccessible = true
+                    return constructor.newInstance(*args) as T
+                } catch (e: Exception) {
+                    // Just ignore an exception and try next constructor
+                    when (e) {
+                        is SQLException -> throw e
+                        else -> logger.trace("", e)
+                    }
+                }
+            }
+
+            // Couldn't find the constructor to use.
+            null
+        } catch (e: Exception) {
+            when (e) {
+                is SQLException -> throw e
+                else -> {
+                    logger.trace("", e)
+                    null
+                }
+            }
+        }
+    }
+
+    @VisibleForTesting
+    internal fun <T : Any> convertAsBean(cls: KClass<T>, resultSet: ResultSet): T? {
+
+        return try {
+            val targetInstance = cls.java.newInstance()
+            val meta = resultSet.metaData
+            val columnNames = (1..meta.columnCount).map { meta.getColumnName(it) }
+
+            // Via methods
+            val properties = Introspector.getBeanInfo(cls.java).propertyDescriptors
+            for (columnName in columnNames) {
+                val property = properties.first { it.name == toLowerCamel(columnName) }
+                val writer = property.writeMethod
+                if (!writer.isAccessible) writer.isAccessible = true
+                writer.invoke(targetInstance, resultSet.getObject(columnName, property.propertyType))
+                // TODO 全てのフィールドが適切に設定されたかチェック
+            }
+            return targetInstance
+
+            // Via fields
+        } catch (e: Exception) {
+            when (e) {
+                is SQLException -> throw e
+                else -> {
+                    logger.info("", e)
+                    null
+                }
+            }
+        }
+    }
+
+    private fun toLowerCamel(str: String): String {
+        return when {
+            str.contains("_") -> LOWER_UNDERSCORE.to(LOWER_CAMEL, str.toLowerCase())
+            str.contains("-") -> LOWER_HYPHEN.to(LOWER_CAMEL, str.toLowerCase())
+            str.all { it.isUpperCase() } -> str.toLowerCase()
+            else -> UPPER_CAMEL.to(LOWER_CAMEL, str)
+        }
     }
 
 }
---
+++ src/test/kotlin/rip/deadcode/asashimo/GeneralResultMapperTest.kt
@@ -0,0 +1,64 @@
+package rip.deadcode.asashimo
+
+import com.google.common.truth.Truth.assertThat
+import org.junit.Test
+import org.mockito.Mockito.`when`
+import org.mockito.Mockito.mock
+import java.sql.ResultSet
+import java.sql.ResultSetMetaData
+
+class GeneralResultMapperTest {
+
+    @Test
+    fun testBasicType1() {
+        val rs = mock(ResultSet::class.java)
+        `when`(rs.getInt(1)).thenReturn(123)
+        val res = GeneralResultMapper.convertToBasicType(Int::class, rs)
+
+        assertThat(res).isEqualTo(123)
+    }
+
+    @Test
+    fun testBasicType2() {
+        val rs = mock(ResultSet::class.java)
+        `when`(rs.getBytes(1)).thenReturn(byteArrayOf(62, 63, 64))
+        val res = GeneralResultMapper.convertToBasicType(ByteArray::class, rs)
+
+        assertThat(res).isEqualTo(byteArrayOf(62, 63, 64))
+    }
+
+    private data class User1(val id: Int, val name: String)
+
+    @Test
+    fun testAllArgConstructor1() {
+        val rs = mock(ResultSet::class.java)
+        val meta = mock(ResultSetMetaData::class.java)
+        `when`(meta.columnCount).thenReturn(2)
+        `when`(rs.metaData).thenReturn(meta)
+        `when`(rs.getObject(1, Int::class.java)).thenReturn(123)
+        `when`(rs.getObject(2, String::class.java)).thenReturn("John")
+        val res = GeneralResultMapper.convertWithAllArgsConstructor(User1::class, rs)!!
+
+        assertThat(res.id).isEqualTo(123)
+        assertThat(res.name).isEqualTo("John")
+    }
+
+    private class User2(var id: Int = 0, var name: String = "")
+
+    @Test
+    fun testBean1() {
+        val rs = mock(ResultSet::class.java)
+        val meta = mock(ResultSetMetaData::class.java)
+        `when`(meta.columnCount).thenReturn(2)
+        `when`(meta.getColumnName(1)).thenReturn("ID")
+        `when`(meta.getColumnName(2)).thenReturn("NAME")
+        `when`(rs.metaData).thenReturn(meta)
+        `when`(rs.getObject("ID", Int::class.java)).thenReturn(123)
+        `when`(rs.getObject("NAME", String::class.java)).thenReturn("John")
+        val res = GeneralResultMapper.convertAsBean(User2::class, rs)!!
+
+        assertThat(res.id).isEqualTo(123)
+        assertThat(res.name).isEqualTo("John")
+    }
+
+}
